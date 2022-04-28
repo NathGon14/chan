f4214cdb5b2221509e706c1b574f304fb3fdf80b
NathGon14 <101176689+NathGon14@users.noreply.github.com>
2022-04-28 16:30:47 +0800

    update

--- app.js
+++ app.js
@@ -1,11 +1,92 @@
 
 const express = require("express")
+const { ExpressPeerServer } = require('peer');
+const { PeerServer } = require('peer');
+const { Server } = require("socket.io");
+const http = require('http');
+const app = express();
 
 const port = process.env.PORT || 5000;
-const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+const serverForPeer = http.createServer(app);
+
+const peerServer = ExpressPeerServer(serverForPeer, {
+  debug: true,
+  path: '/'
+});
+
+app.use('/peerjs', peerServer);
+
+peerServer.on('connection', (client) => { 
+// console.log(client);
+  
+
+});
+
+
+io.on("connection", (socket) => {
+ 
+    socket.on("joining",(param)=>{
+    
+        socket.join(getRoom(socket))
+        socket.to(getRoom(socket)).emit("peerId",param)
+   
+    })
+
+    socket.on("sendMessage",(messagePackage)=>{
+    
+
+      socket.to(getRoom(socket)).emit("createMessage",messagePackage)
+ 
+  })
+  socket.on("sendToggle",(toggleOptions)=>{
+  
+    socket.to(getRoom(socket)).emit("toggle",toggleOptions)
+
+})
+
+
+
+  
+
+
+  socket.on("disconnecting", (reason) => {
+
+  socket.to(getRoom(socket)).emit("leave",socket.id)
+
+
+
+  });
+  });
+  
+  function getRoom(socket){
+
+    const clientSocket  = socket.rooms.values()
+      clientSocket.next().value
+        const clientRoom = clientSocket.next().value
+
+
+     return clientRoom;
+
+  }
+
+
+
+
+
+
+
+
+
+
 
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
+
 
 
 
@@ -15,8 +96,29 @@ res.sendFile(__dirname+"/public/index.html")
 
 })
 
-app.listen(port,()=>{
+app.get("/room/:id",(req,res)=>{
 
-console.log("listening")
+    res.sendFile(__dirname+"/views/video.html")
+
+    
+    })
+
+
+
+    
+
+
+server.listen(port,()=>{
+
+
 
 })
+
+serverForPeer.listen(9000,()=>{
+
+    console.log("porrt")
+    
+ })
+
+
+  
---
+++ public/script.js
@@ -0,0 +1,756 @@
+
+
+  let cameraPermmited = false;
+  let micPermitted = false;
+  let numberOfUser = 1;
+  let localData = [];
+  var myLocalStream = ""
+  let username 
+  const socket = io();
+
+
+
+
+checkCookie()
+
+
+
+function checkCookie() {
+  let user = getCookie("username");
+  if (user == "")
+         askForName()
+     else
+     username = user
+
+}
+
+
+
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+
+
+  function askForName(){
+
+    let person = prompt("Please enter your name");
+   
+    if (person == null || person == ""){
+      person = "Guest_"+(Math.floor(Math.random()*90000) + 10000);
+      if(getCookie("username") != ""){
+        username = getCookie("username");
+        return
+      }
+
+    }
+  
+    username = person;
+    setCookie("username", username, 7)
+  }
+
+  function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  
+  
+  
+
+  
+const peer = new Peer({
+    host: '/',
+    port: 9000,
+    path: '/peerjs'
+  });
+
+
+//events
+
+$(".chooser").on("click",(e)=>{
+  const parent = $(e.target).closest(".chooser")
+
+  $(".chooser").toArray().forEach(element => {
+    
+    $(element).removeClass("active")//reset the active
+    const id = $(element).attr("data-href")
+    $(id).css("display","none")
+  });
+
+ 
+  const id = $(parent).attr("data-href")
+
+  $(parent).addClass("active")
+
+  $(id).css("display","flex")
+
+
+  if($("#chat").children().length > 0)  $("#chat").children().last()[0].scrollIntoView()
+
+
+  resetNotif();
+
+
+})
+let time = ""
+$(".video-conatiner").on("click",(e)=>{
+  const target = e.target
+if(target.id == "forward" || target.id == "backward" )return
+
+clearTimeout(time)
+$(".overlay").fadeIn().css("display","grid")
+
+time = setTimeout(function(e){
+
+  $(".overlay").fadeOut()
+
+},10000)
+
+
+})
+$(".overlay").on("click",(e)=>{
+  if(e.target != $(".overlay")[0])return
+  clearTimeout(time)
+  $(".overlay").css("display","none")
+})
+
+$("#forward").on("click",(e)=>{
+console.log("forward")
+  moveforward();
+
+})
+$("#backward").on("click",(e)=>{
+  console.log("backward")
+    moveback();
+  
+  })
+
+ 
+
+
+
+
+$("#messageBox").on("click",(e)=>{
+
+
+
+  $( "#sideBody" ).css({display:"flex"}).animate({
+      left:"0"
+  })
+
+  //reset the message notif
+  resetNotif();
+
+
+
+})
+
+$("#send").on("click",(e)=>{
+
+  chat()
+
+})
+
+
+$("#back").on("click",(e)=>{
+
+$( "#sideBody" ).animate({
+  left:"-100%",
+  display:"none"
+  
+},function(e){
+  $( "#sideBody" ).css({display:"none"})
+})
+
+})
+
+
+  let userID = ""
+
+socket.on("connect", () => {
+
+  userID = socket.id
+
+  });
+
+socket.on("createMessage",(message)=>{
+
+createMessage(message)
+checkIfchatboxIsHidden();
+
+
+})
+
+socket.on("leave",(theirID)=>{
+
+  userLeave(theirID);
+  
+  })
+  socket.on("toggle",(options)=>{
+
+   toggleVideoSettings(options["what"],options["ID"])
+    
+  })
+
+ 
+
+
+
+peer.on("open", (id)=>{
+  localData = [];
+
+  localData.push({name:username,userID:userID})
+  createUsers({name:username,userID:userID})
+
+joining(peer,id,socket,null)
+
+
+})
+
+
+
+
+function userLeave(id){
+//remove the video of the one who leave
+ const leaverVideo = $("video").toArray().filter((e)=>{ return $(e).data("data-id") == id})[0]
+
+ if($(leaverVideo).closest(".video-wrapper")!=null) $(leaverVideo).closest(".video-wrapper")[0].remove()
+  
+const leaverName = localData.find((e)=> e["userID"] == id)
+ createNotifcation({message:leaverName["name"]+" has left",leave:true})
+ localData = localData.filter((e)=> e["userID"] != id)
+  numberOfUser--;
+  pickGridLeave()
+
+  removeUsers(id)
+}
+let call;
+let peerConnections = [];
+
+
+// on open will be launch when you successfully connect to PeerServer
+
+let facingMode = "environment"
+let contraints =()=>{
+
+  if(facingMode == "environment"){
+    facingMode = "user"
+  }else{
+    facingMode = "environment"
+  }
+  return {facingMode:facingMode}
+
+}
+function cameraSwitch(){
+
+  if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
+    // console.log(peerConnections)
+    // console.log(peer)
+
+  }const getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+  getUserMedia({video:contraints, audio: true}, function(stream) {
+
+    vide.srcObject = stream
+
+
+  })
+
+
+
+}
+
+
+
+// first joingin
+socket.on("peerId",(param)=>{
+const ID = param["userID"]
+ connection = peer.connect(param["peerId"]);
+
+  numberOfUser++;
+  localData.push({name:param["name"],userID:param["userID"]})
+  createUsers(param)
+  createNotifcation({message:param["name"]+" has Joined",leave:false})
+  caller = peer.call(param["peerId"], myLocalStream,{ metadata: { "ID": userID,"name":username }});
+   
+     peerConnections.push(caller)
+  
+  let preventEmit = 0;
+  caller.on('stream', function(theirStream) {
+    // perform swap if your the only two 
+    //two emit preventor
+       if(preventEmit != 0)return
+
+   swapVideo(myLocalStream,theirStream,ID)
+
+      pickGrid(theirStream,ID)
+      console.log(localData)
+
+          preventEmit++
+ 
+   });
+ 
+
+
+})
+
+
+
+
+async function joining(peer,peerID,socket){
+
+const getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+getUserMedia({video: {facingMode:"user"}, audio: true}, function(stream) {
+  myLocalStream = stream;
+
+  createVideoWrapper(stream,userID)
+  peer.on('call', function(call) {
+    numberOfUser++;
+    caller = call
+    peerConnections.push(caller)
+
+  call.answer(stream); 
+  let preventEmit = 0;
+  call.on('stream', function(theirStream) {
+      if(preventEmit != 0)return  
+      const ID = call.metadata.ID
+      
+      createUsers({name:call.metadata.name,userID:ID})
+      localData.push({name:call.metadata.name,userID:ID})
+      console.log(localData)
+      swapVideo(stream,theirStream,ID)
+
+      pickGrid(theirStream,ID)
+
+       preventEmit++
+    
+
+  });
+  
+  
+  });
+  socket.emit("joining",{peerId:peerID,name:username,userID:userID})
+
+}, function(err) {
+
+
+  console.log('permisionNotu' ,err);
+});
+
+
+}
+
+function swapVideo(mystream,theirStream,ID){
+  //its only my stream threre
+if($(videoContainer).children().length > 1) return
+
+
+$(".video-wrapper  video")[0].srcObject = theirStream
+$(".video-wrapper  video").data("data-id",ID)
+
+createMyStream(mystream,userID)
+
+
+
+
+}
+
+
+// carousel script
+
+
+
+function moveforward(){
+let limit = numberOfGrid;
+if(numberOfGrid == 5){
+
+  limit = 1;
+}
+
+
+  //append the first child to last
+  for(let i = 0; i< limit; i++){
+    let firstChild = $(videoContainer).children().first()
+    if(limit == 1 && numberOfGrid != 5 && numberOfUser >= 2)firstChild = $(videoContainer).children()[1]
+
+    
+    $(videoContainer).append(firstChild);
+
+  }
+
+
+}
+
+
+
+function moveback(){
+  const limit = numberOfGrid;
+ for(let i = 0; i< limit; i++){
+    let firstChild = $(videoContainer).children().last()
+
+    $(videoContainer).prepend(firstChild);
+    if(limit == 1 &&  numberOfGrid != 5 &&  numberOfUser >= 2)$(videoContainer).prepend($('.my-video'));
+
+  }
+
+}
+
+// toggle settings
+
+function toggleVideoSettings(what,ID){
+  const video = $("video").toArray().filter((e)=>{ return $(e).data("data-id") == ID})[0]
+  let icon = $(video).parent().find("div i:nth-child(1)")
+  if(what =="camera") icon= $(video).parent().find("div i:nth-child(2)")
+
+  $(icon).toggle()
+ 
+}
+
+function turnOffCamera(element){
+
+  toggleVideoSettings("camera",userID)
+
+  socket.emit("sendToggle",{what:"camera",ID:userID})
+
+  $(element).removeClass()
+
+  if(myLocalStream.getVideoTracks()[0].enabled){
+  myLocalStream.getVideoTracks()[0].enabled = false
+  $(element).addClass("fa-solid fa-video-slash")
+
+    return
+  }
+  $(element).addClass("fa-solid fa-video")
+  myLocalStream.getVideoTracks()[0].enabled = true
+
+}
+
+function turnOffMic(element){
+  toggleVideoSettings("mic",userID)
+  socket.emit("sendToggle",{what:"mic",ID:userID})
+
+  $(element).removeClass()
+  if(myLocalStream.getAudioTracks()[0].enabled){
+  myLocalStream.getAudioTracks()[0].enabled = false
+  $(element).addClass("fa-solid fa-microphone-slash")
+
+    return
+  }
+
+  $(element).addClass("fa-solid fa-microphone")
+  myLocalStream.getAudioTracks()[0].enabled = true
+
+}
+
+
+
+let numberOfGrid = 1;
+
+
+function hideOtherGrid(className){
+
+  $(videoContainer).removeClass()
+  $(videoContainer).addClass(className)
+
+
+}
+let nakapiliba= false
+
+function manualPickGrid(){
+  $(".my-video").show();
+ 
+  numberOfGrid++;
+  if(numberOfGrid == 1){
+   console.log("grid 1")
+    hideOtherGrid("video-grid-1")
+    $(videoContainer).prepend($(".my-video"));
+    nakapiliba = true;
+   
+  }
+  if(numberOfGrid == 2 && numberOfUser >=2){
+    console.log("grid 2")
+    hideOtherGrid("video-grid-2")
+    nakapiliba = true;
+  
+  }
+
+  if(numberOfGrid == 3 && numberOfUser >= 3){
+    console.log("grid 3")
+    hideOtherGrid("video-grid-3")
+    nakapiliba = true;
+    
+
+  }
+  if(numberOfGrid == 4 && numberOfUser >= 4){
+    console.log("grid 4")
+    hideOtherGrid("video-grid-4")
+    nakapiliba = true;
+  
+
+  }
+  if(numberOfGrid == 5){
+    console.log("grid 5")
+    hideOtherGrid("video-grid-5")
+    nakapiliba = true;
+  
+
+  }
+
+
+  if(numberOfGrid >= 6) numberOfGrid = 0;
+  if(!nakapiliba) manualPickGrid()
+  if(nakapiliba) nakapiliba = false
+   
+
+  
+
+
+ 
+
+
+
+ 
+
+
+}
+
+function pickGridLeave(){
+
+  switch(numberOfUser){
+         
+    case 1:
+      $(videoContainer).removeClass()
+      $(videoContainer).addClass("video-grid-1")
+      $(videoContainer).children().first().removeClass().addClass("video-wrapper")
+      numberOfGrid = 1;
+
+    break
+    case 2:
+      $(videoContainer).removeClass()
+      $(videoContainer).addClass("video-grid-1")
+      numberOfGrid = 2;
+    break
+
+    case 3:
+    $(videoContainer).removeClass()
+    $(videoContainer).addClass("video-grid-3")
+    numberOfGrid = 3;
+
+
+    break;
+
+    default:
+    $(videoContainer).removeClass()
+    $(videoContainer).addClass("video-grid-4")
+    numberOfGrid = 4;
+    
+    break;
+
+
+   } 
+
+
+}
+
+function createNotifcation(message){
+
+createMessage({sender:"Server",message:message["message"],name:"Server"})
+
+let div = $("<div style = 'display:none;' class = 'color1'><span></span></div>")
+if(message['leave'])div = $("<div style = 'display:none;' class = 'color2'><span></span></div>")
+$(div).find("span").text(message["message"])
+$("#notification-up").append(div)
+$(div).on("click",()=>$(div).remove())
+
+$(div).slideDown( 300, function() {
+setTimeout(()=>$(this).remove(),2000)
+});
+
+}
+
+
+
+function pickGrid(theirStream,ID){
+  if(numberOfUser == 2) return
+  if(ID != null)    createVideoWrapper(theirStream,ID)
+
+     switch(numberOfUser){
+
+      case 3:
+      $(videoContainer).removeClass()
+      $(videoContainer).addClass("video-grid-3")
+      numberOfGrid = 3;
+
+
+      break;
+
+      default: 
+      $(videoContainer).removeClass()
+      $(videoContainer).addClass("video-grid-1")
+      numberOfGrid = 4;
+      
+      break;
+
+
+     } 
+
+
+}
+
+function createVideoWrapper(stream,ID){
+  const divElement = document.createElement("div")
+  const anotherDiv = "<div><div><i class='fa-solid fa-microphone-slash'></i><i class='fa-solid fa-video-slash'></i></div></div>"
+
+
+  $(divElement).addClass("video-wrapper")
+  $(divElement).html(anotherDiv)
+
+
+  const videoelemet = document.createElement("video")
+  $(videoelemet).attr("autoplay",true)
+ 
+  if(ID != null)  $(videoelemet).data("data-id",ID)
+
+  videoelemet.srcObject = stream
+ 
+  $(divElement).append(videoelemet)
+ 
+  $(videoContainer).append(divElement)
+
+}
+
+function createMyStream(stream,ID){
+
+  const divElement = document.createElement("div")
+  const anotherDiv = "<div><div><i class='fa-solid fa-microphone-slash'></i><i class='fa-solid fa-video-slash'></i></div></div>"
+  $(divElement).html(anotherDiv)
+  $(divElement).addClass("my-video")
+
+
+  const videoelemet = document.createElement("video")
+  $(videoelemet).attr("autoplay",true)
+  $(videoelemet).data("data-id",ID)
+  
+  videoelemet.srcObject = stream
+ 
+  $(divElement).append(videoelemet)
+ 
+  $(videoContainer).prepend(divElement)
+
+
+
+
+}
+
+function createUsers(data){
+  let time = new Date();
+  time = time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+  const object =    $("<div class='user'><div><i class='fa-solid fa-user'></i></div><div><div><span>Joined: "+time+"</span></div><div><span></span></div></div></div>")
+  const span = $(object).find("div:nth-child(2) > div:nth-child(2) span")
+  $(span).text("Name: "+data["name"])
+  $(object).data("data-id",data["userID"])
+  console.log(data["userID"])
+  $("#users").append($(object))
+  $("#numberUsers").text(localData.length)
+
+}
+
+function removeUsers(id){
+ $("#users").children().toArray().filter((e)=> $(e).data("data-id")==id)[0].remove()
+ $("#numberUsers").text(localData.length)
+
+}
+
+
+
+function createMessage(message){
+  // message["sender"] ="me"
+  let time = new Date();
+  time = time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+
+
+ const messageObject = "<div class ='chat'><div><span></span></div><div><span></span></div><div><span>"+time+"</span></div></div>"
+
+ const html = $.parseHTML(messageObject)
+ $("#chat").append(html)
+
+ switch(message["sender"]){
+
+  case "me":
+      $(html).children().first().children().first().css("color","#2f59f5").text(message["name"])
+
+  break;
+
+  case "them":
+  $(html).children().first().children().first().css("color","black").text(message["name"])
+
+  break;
+
+  default:
+
+  $(html).children().first().children().first().css("color","red").text(message["name"])
+
+ }
+$(html).children("div:nth-child(2)").children().first().text(message["message"])
+
+$(html)[0].scrollIntoView()
+
+
+}
+
+function chat(){
+
+  let message = $("#message").val()
+  message = message.replace(/(\r\n|\r|\n){2}/g, '$1').replace(/(\r\n|\r|\n){3,}/g, '$1\n');
+  
+  if(message.trim() =="" || message.trim() ==null){
+  console.log("spaces only");
+  return
+  }
+
+const data = {sender:"me",message:message,name:username}
+  createMessage(data)
+
+  data["sender"]="them"
+
+socket.emit("sendMessage",data)
+$("#message").val("")
+$("#message")[0].focus()
+
+}
+
+function checkIfchatboxIsHidden(){
+  if($("#chatbox").css("display") == "flex" && $("#sideBody").css("display") == "flex")return
+
+  //creating notif 
+
+ const numberofmessage =  Number($("#messageNotif span").first().text())+1
+ console.log(numberofmessage)
+ $("#messageNotif span").first().text(numberofmessage)
+ $("#messageNotif").show()
+
+ 
+}
+
+function resetNotif(){
+
+  if($("#chatbox").css("display") != "flex")return
+  $("#messageNotif").hide()
+  $("#messageNotif span").first().text(0)
+}
